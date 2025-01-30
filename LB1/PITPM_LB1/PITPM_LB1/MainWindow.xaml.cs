7340a9a1578adac58484e5540275cbaed91d1301
druap <76074150+d404mer@users.noreply.github.com>
2025-01-30 19:00:11 +0300

    init laba 1 & finish

---
+++ LB1/PITPM_LB1/PITPM_LB1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PITPM_LB1.SortingSSSS;
+
+namespace PITPM_LB1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BubbleSort_Click(object sender, RoutedEventArgs e)
+        {
+            int[] numbers = ParseInput();
+            if (numbers == null) return;
+            BubbleSort.Sort(numbers);
+            OutputArray.Text = string.Join(", ", numbers);
+        }
+
+        private void InsertionSort_Click(object sender, RoutedEventArgs e)
+        {
+            int[] numbers = ParseInput();
+            if (numbers == null) return;
+            InsertionSort.Sort(numbers);
+            OutputArray.Text = string.Join(", ", numbers);
+        }
+
+        private void QuickSort_Click(object sender, RoutedEventArgs e)
+        {
+            int[] numbers = ParseInput();
+            if (numbers == null) return;
+            QuickSort.Sort(numbers, 0, numbers.Length - 1);
+            OutputArray.Text = string.Join(", ", numbers);
+        }
+
+
+        private int[] ParseInput()
+        {
+            try
+            {
+                return InputArray.Text.Split(',').Select(s => int.Parse(s.Trim())).ToArray();
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод! Введите числа через запятую.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        private void ClearInput_Click(object sender, RoutedEventArgs e)
+        {
+            InputArray.Text = "";
+            OutputArray.Text = "";
+        }
+
+        private void ClearOutput_Click(object sender, RoutedEventArgs e)
+        {
+            OutputArray.Text = "";
+        }
+    }
+}
---
+++ LB1/PITPM_LB1/TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+using PITPM_LB1;
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
