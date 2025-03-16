b00f208f83e613e2261b9fe31ccb4ada1c748a4e
d404mer <dev1lc3st@gmail.com>
2025-03-16 16:43:09 +0300

    auth

--- LB3 + db/LB3_Market/LB3_Market/App.xaml.cs
+++ LB3 + db/LB3_Market/LB3_Market/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using LB3_Market.Models;
 
 namespace LB3_Market
 {
@@ -13,5 +14,17 @@ namespace LB3_Market
     /// </summary>
     public partial class App : Application
     {
+        public const string ROLE_ADMIN = "Admin";
+        public const string ROLE_USER = "User";
+
+        public static void SetCurrentUser(Users user)
+        {
+            if (user != null)
+            {
+                user.PasswordHash = null;
+            }
+
+            Current.Resources["CurrentUser"] = user;
+        }
     }
 }
